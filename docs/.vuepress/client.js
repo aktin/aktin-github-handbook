3f806100292795c209faaa4b2137f708bd087134
akomii <alexkombie@gmail.com>
2025-08-28 12:52:34 +0200

    WIP: Custom Footer

---
+++ docs/.vuepress/client.js
@@ -0,0 +1,6 @@
+import { defineClientConfig } from 'vuepress/client'
+import CustomFooter from './components/CustomFooter.vue'
+
+export default defineClientConfig({
+  rootComponents: [CustomFooter],
+})
---
+++ docs/.vuepress/components/CustomFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="custom-footer">
+    <p>© 2025 AKTIN – All rights reserved</p>
+  </footer>
+</template>
+
+<style>
+.custom-footer {
+  margin-top: 2rem;
+  padding: 1rem;
+  text-align: center;
+  border-top: 1px solid #ddd;
+  color: #666;
+}
+</style>
